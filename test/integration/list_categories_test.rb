bbccd5bdb6683c4b260ea93fa7ae129c207276f0
Rails Udemy Course <sufyan@shispare.com>
2017-03-15 19:16:39 +0000

    fixed list categories test bug

--- test/integration/list_categories_test.rb
+++ test/integration/list_categories_test.rb
@@ -1,9 +1,9 @@
 require 'test_helper'
 
-class CreateCategoriesTest < ActionDispatch::IntegrationTest
+class ListCategoriesTest < ActionDispatch::IntegrationTest
     
     def setup
-       @category = Category.create(name: "books")
+       @category = Category.create(name: "sports")
        @category2 = Category.create(name: "program")
     end
     
@@ -13,5 +13,4 @@ class CreateCategoriesTest < ActionDispatch::IntegrationTest
         assert_select "a[href=?]", category_path(@category), text: @category.name
         assert_select "a[href=?]", category_path(@category2), text: @category2.name
     end
-   
 end
