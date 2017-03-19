7c9b74d7cf8c7a053c845daa5909950b5b6066da
Rails Udemy Course <sufyan@shispare.com>
2017-03-19 22:01:39 +0000

    update all views with categories

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,6 @@
 class Category < ActiveRecord::Base
-validates :name, presence: true, length: { minimum: 3, maximum: 30 }
-validates_uniqueness_of :name
+    has_many :article_categories
+    has_many :articles, through: :article_categories
+    validates :name, presence: true, length: { minimum: 3, maximum: 30 }
+    validates_uniqueness_of :name
 end
